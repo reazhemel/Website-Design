a1b7b42c1f641dd2082fa739cbb5dc9415ab8d1c
Hemel <47193108+reazhemel@users.noreply.github.com>
2022-03-13 00:02:42 +0600

    Add files via upload

---
+++ Portfolio Design-/js/script.js
@@ -0,0 +1,28 @@
+$('.testimonials').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+  prevArrow: '<i class="fas fa-angle-double-left left_arrow"></i>',
+  nextArrow: '<i class="fas fa-angle-double-right right_arrow"></i>',
+
+});
+
+
+// counter
+
+// import counterUp from'counterup2'
+// counterUp(document.querySelector('.counter') {
+//     duration: 1000,
+//     delay: 16
+//   })
+//   const el = document.querySelector('.counter')
+
+// new Waypoint( {
+//     element: el,
+//     handler:function() {
+//       counterUp( el )
+//       this.destroy()
+//     },
+//     offset:'bottom-in-view'
+// })
